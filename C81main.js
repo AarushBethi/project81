781e6c80192fbc8f4ecb6ccb318aa6431c40f524
AarushBethi <128331842+AarushBethi@users.noreply.github.com>
2023-09-25 19:54:19 -0400

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,34 @@
+canvas = document.getElementById("myCanvas")
+color = "red"
+ctx = canvas.getContext("2d")
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth=2
+ctx.arc(200,200,40,0,2*Math.PI)
+ctx.stroke()
+ctx.beginPath()
+ctx.strokeStyle = "green"
+ctx.lineWidth=2
+ctx.arc(300,200,40,0,2*Math.PI)
+ctx.stroke()
+ctx.beginPath()
+ctx.strokeStyle = "blue"
+ctx.lineWidth=2
+ctx.arc(400,200,40,0,2*Math.PI)
+ctx.stroke()
+ctx.beginPath()
+ctx.strokeStyle = "yellow"
+ctx.lineWidth=2
+ctx.arc(250,250,40,0,2*Math.PI)
+ctx.stroke()
+ctx.beginPath()
+ctx.strokeStyle = "orange"
+ctx.lineWidth=2
+ctx.arc(350,250,40,0,2*Math.PI)
+ctx.stroke()
+canvas.addEventListener("mousedown", my_mousedown)
+
+function clearcanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+
+}
